d5a3f0351e7ac1f4c1a7ab2bec49154d1d33e10c
RafaelMacena <rafaelsouzasouza1945@gmail.com>
2022-11-11 16:21:57 -0300

    esboço inicial pra resolver o exercicio printar o z na tela

---
+++ src/br/com/magnasistema/modelo/ManipulanoArrayComLacos.java
@@ -0,0 +1,23 @@
+package br.com.magnasistema.modelo;
+
+public class ManipulanoArrayComLacos {
+
+	public static void main(String[] args) {
+
+		char[] z = new char[12];
+		
+		
+		 
+		for (int i = 0; i < z.length; i++) {
+		      if (i == z.length) {
+				
+			}
+			 for (int j = 0; j < z.length; j++) {
+				   System.out.print("Z");
+				}
+			
+		}
+
+	}
+
+}
