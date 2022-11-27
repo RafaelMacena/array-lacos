b46e08c68aeaef0730d9adf733e26c750b85f3b3
RafaelMacena <rafaelsouzasouza1945@gmail.com>
2022-11-27 12:11:59 -0300

    alterando nome da matriz

--- src/br/com/magnasistema/modelo/LetraX.java
+++ src/br/com/magnasistema/modelo/LetraX.java
@@ -4,10 +4,10 @@ public class LetraX {
 
 	public static void main(String[] args) {
 
-		int[][] z = new int[10][10];
+		int[][] dimensao = new int[10][10];
 
-		for (int i = 0; i < z.length; i++) {
-			for (int j = 0; j < z.length; j++) {
+		for (int i = 0; i < dimensao.length; i++) {
+			for (int j = 0; j < dimensao.length; j++) {
 				if ((i == 0 || i == 9) || (j == 0 || j == 9) || (i == 1 && j == 8)
 						|| (i == 2 && j == 7) || (i == 3 && j == 6) || (i == 4 && j == 5) || (i == 5 && j == 4)
 						|| (i == 6 && j == 3) || (i == 7 && j == 2) || (i == 8 && j == 1) || (i == 1 && j == 1)
